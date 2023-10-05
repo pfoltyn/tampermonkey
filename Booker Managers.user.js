d87a72b1bd13956b272e7a7e1ac233e8938e8f64
Piotr Foltyn <piotr.foltyn@gmail.com>
2023-10-05 16:33:04 +0100

    Add button to copy all DMs' emails

--- Booker Managers.user.js
+++ Booker Managers.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Booker Managers
 // @namespace    https://github.com/pfoltyn/tampermonkey
-// @version      0.3
+// @version      0.4
 // @description  Add managers button
 // @author       Piotr Foltyn
 // @match        http*://booker.eventmapsolutions.com/*
@@ -11,9 +11,11 @@
 
 (function() {
     'use strict';
+
+    const api_url = "https://booker.eventmapsolutions.com/api/";
     var timer_id = null;
     var managers = null;
-    var refresh_cnt = 0
+    var refresh_cnt = 0;
 
     function httpGetAsync(theUrl, callback) {
         var xmlHttp = new XMLHttpRequest();
@@ -30,7 +32,9 @@
         managers = new Map();
         const arr = JSON.parse(msg);
         arr.forEach(function(e) {
-            managers.set(e.OptimeIndex, `${e.Forename} ${e.Surname} ${e.Id}@cam.ac.uk`);
+            if (e.Id != null && /^\w{2,5}\d{2,6}$/.test(e.Id.trim())) {
+                managers.set(e.OptimeIndex, [e.Forename, e.Surname, `${e.Id.trim()}@cam.ac.uk`]);
+            }
         });
     }
 
@@ -38,7 +42,10 @@
         var display = "";
         const arr = JSON.parse(msg);
         arr.forEach(function(e) {
-            display += managers.get(e) + "\n";
+            const man = managers.get(e)
+            if (man != undefined) {
+                display += `${man[0]} ${man[1]} ${man[2]}\n`;
+            }
         });
         if (arr.length == 0) {
             display = "No managers selected for that department.";
@@ -47,7 +54,36 @@
         alert(display);
     }
 
-    function insertManagersButton () {
+    function insertEmailAllManagersButton() {
+        var div = document.getElementById("bookerDepartmentTable_filter");
+        if (div.children.length == 2) {
+            var node = document.createElement("button");
+            node.classList.add("crudActionButton");
+            node.title = "Copy All DMs' Emails";
+            node.style.color = "white";
+            node.addEventListener("click", function(e) {
+                var emails = "";
+                for (let value of managers.values()) {
+                    emails += `${value[2]};`;
+                }
+                GM.setClipboard(emails.slice(0, -1));
+                alert("Emails copied to clipboard.");
+            }, false);
+
+            var span = document.createElement("span");
+            span.classList.add("hidden-xs");
+            span.textContent = "Copy All DMs' Emails";
+            node.appendChild(span);
+
+            var i = document.createElement("i");
+            i.classList.add("osci", "osci-email");
+            node.appendChild(i);
+
+            div.appendChild(node);
+        }
+    }
+
+    function insertManagersButtons() {
         var table = document.getElementById("bookerDepartmentTable");
         for (var i = 0, row; row = table.rows[i]; i++) {
             var cell = row.cells[row.cells.length - 1];
@@ -57,17 +93,26 @@
                 node.title = "Show Managers";
                 node.addEventListener("click", function(e) {
                     const data_id = e.target.getAttribute("data-id");
-                    httpGetAsync("https://booker.eventmapsolutions.com/api/booker/departmentOwnership/department/" + data_id, showMessage);
+                    httpGetAsync(`${api_url}booker/departmentOwnership/department/${data_id}`, showMessage);
                 }, false);
                 node.setAttribute("data-id", cell.children[0].getAttribute("data-id"));
                 cell.insertBefore(node, cell.children[0]);
             }
         }
+    }
+
+    function refreshManagers() {
         refresh_cnt++;
         if (refresh_cnt >= 60) {
             refresh_cnt = 0;
-            httpGetAsync("https://booker.eventmapsolutions.com/api/staff/getDepartmentManagers", parseManagers);
+            httpGetAsync(`${api_url}staff/getDepartmentManagers`, parseManagers);
         }
+    }
+
+    function timerCallback() {
+        insertEmailAllManagersButton();
+        insertManagersButtons();
+        refreshManagers();
     }
 
     window.addEventListener('hashchange', function (e) {
@@ -79,15 +124,15 @@
             return;
         }
         if (timer_id == null) {
-            timer_id = setInterval(insertManagersButton, 1000);
+            timer_id = setInterval(timerCallback, 1000);
         }
     });
 
     if (window.location.hash == "#crud/departments") {
-        httpGetAsync("https://booker.eventmapsolutions.com/api/staff/getDepartmentManagers", parseManagers);
+        httpGetAsync(`${api_url}staff/getDepartmentManagers`, parseManagers);
 
         if (timer_id == null) {
-            timer_id = setInterval(insertManagersButton, 1000);
+            timer_id = setInterval(timerCallback, 1000);
         }
     }
 })();
